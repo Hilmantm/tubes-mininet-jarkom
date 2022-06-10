9fd33a67dd0d51f4257efcbc5978cdf3b290bf2a
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2022-06-11 01:46:08 +0700

    Add connection for host and router (not yet)

--- main.py
+++ main.py
@@ -3,48 +3,10 @@ from mininet.net import Mininet
 from mininet.node import Node
 from mininet.cli import CLI
 from mininet.link import TCLink
-from mininet.log import setLogLevel
+from mininet.log import setLogLevel, info
 import os
 import time
 from datetime import datetime
-
-# class NetworkTopology(Topo):
-
-#     def build(self, **_opts):
-
-#         # Konfigurasi host
-#         host_a = self.addHost('hostA', ip="192.168.20.2/24", defaultRoute="192.168.20.1")
-#         host_b = self.addHost('hostB', ip="192.168.50.2/24", defaultRoute="192.168.50.1")
-
-#         # Konfigurasi router
-#         router_1 = self.addHost('router1', ip="192.168.1.1/24")
-#         router_2 = self.addHost('router2', ip="192.168.2.1/24")
-#         router_3 = self.addHost('router3', ip="192.168.3.1/24")
-#         router_4 = self.addHost('router4', ip="192.168.4.1/24")
-
-#         #Mendefinisikan bandwidth (/Mbps)
-#         bandwidth1 = {'bw':1}
-#         bandwidth2 = {'bw':0.5}
-
-#         # add link host
-#         self.addLink(router_1, host_a, intfName1 = 'router1-eth0', intfName2 = 'hostA-eth0', cls=TCLink, **bandwidth1)
-#         self.addLink(router_2, host_a, intfName1 = 'router2-eth0', intfName2 = 'hostA-eth1', cls=TCLink, **bandwidth1)
-#         self.addLink(router_3, host_b, intfName1 = 'router3-eth0', intfName2 = 'hostB-eth0', cls=TCLink, **bandwidth1)
-#         self.addLink(router_4, host_b, intfName1 = 'router4-eth0', intfName2 = 'hostB-eth1', cls=TCLink, **bandwidth1)
-
-#         # add link router
-#         self.addLink(router_1, router_3, intfName1 = 'router1-eth1', intfName2 = 'router3-eth1', cls=TCLink, **bandwidth2)
-#         self.addLink(router_1, router_4, intfName1 = 'router1-eth2', intfName2 = 'router4-eth1', cls=TCLink, **bandwidth1)
-#         self.addLink(router_2, router_4, intfName1 = 'router2-eth2', intfName2 = 'router4-eth2', cls=TCLink, **bandwidth2)
-#         self.addLink(router_2, router_3, intfName1 = 'router2-eth1', intfName2 = 'router3-eth2', cls=TCLink, **bandwidth1)
-#         self.build()
-
-# def run():
-#     net = Mininet(topo=NetworkTopology())
-#     net.start()
-
-# setLogLevel('info')
-# run()
 
 def configNetwork():
 
@@ -57,14 +19,61 @@ def configNetwork():
     net.addHost( 'r3', ip='192.168.3.1/24' )
     net.addHost( 'r4', ip='192.168.4.1/24' )
     
-    # Add Host h0,h1,h2,h3
-    net.addHost("ha", ip="192.168.20.2/24", defaultRoute="via 192.168.20.1")
-    net.addHost("hb", ip="192.168.50.2/24", defaultRoute="via 192.168.50.1")
+    # Add Host ha, hb
+    net.addHost("ha", ip="192.168.1.2/24", defaultRoute="via 192.168.1.1")
+    net.addHost("hb", ip="192.168.2.2/24", defaultRoute="via 192.168.2.1")
     
 
     # Add Link
     net.addLink( net[ 'ha' ], net[ 'r1' ], intfName2='r1-eth0', bw=1 ) 
-    net.addLink( net[ 'ha' ], net[ 'r2' ], intfName2='r2-eth0', bw=1 )
+    net.addLink( net[ 'ha' ], net[ 'r3' ], intfName2='r3-eth0', bw=1 )
+    net.addLink( net[ 'hb' ], net[ 'r2' ], intfName2='r2-eth0', bw=1 )
+    net.addLink( net[ 'hb' ], net[ 'r4' ], intfName2='r4-eth0', bw=1 )
+
+    net.addLink( net[ 'r1' ], net[ 'r2' ], intfName2='r2-eth1', bw=0.5 )
+    net.addLink( net[ 'r1' ], net[ 'r4' ], intfName2='r4-eth1', bw=1 )
+    net.addLink( net[ 'r3' ], net[ 'r2' ], intfName2='r2-eth2', bw=1 )
+    net.addLink( net[ 'r3' ], net[ 'r4' ], intfName2='r4-eth2', bw=0.5 )
+
+
+    # add IP Address for host A
+    net[ 'ha' ].cmd( 'ip addr add 192.168.1.2/24 brd + dev ha-eth0' )
+    net[ 'ha' ].cmd( 'ip addr add 192.168.2.2/24 brd + dev ha-eth1' )
+
+    # add IP Address for host B
+    net[ 'ha' ].cmd( 'ip addr add 192.168.3.1/24 brd + dev hb-eth0' )
+    net[ 'ha' ].cmd( 'ip addr add 192.168.3.2/24 brd + dev hb-eth1' )
+
+    # add IP Address for router 1
+    net[ 'r1' ].cmd( 'ip addr add 192.168.1.1/24 brd + dev r1-eth0' )
+    net[ 'r1' ].cmd( 'ip addr add 192.168.2.1/24 brd + dev r1-eth1' )
+    net[ 'r1' ].cmd( 'ip addr add 192.168.4.1/24 brd + dev r1-eth2' )
+
+    # add IP Address for router 2
+    net[ 'r2' ].cmd( 'ip addr add 192.168.2.1/24 brd + dev r2-eth0' )
+    net[ 'r2' ].cmd( 'ip addr add 192.168.1.1/24 brd + dev r2-eth1' )
+    net[ 'r2' ].cmd( 'ip addr add 192.168.3.1/24 brd + dev r2-eth2' )
+
+    # add IP Address for router 3
+    net[ 'r3' ].cmd( 'ip addr add 192.168.3.1/24 brd + dev r3-eth0' )
+    net[ 'r3' ].cmd( 'ip addr add 192.168.2.1/24 brd + dev r3-eth1' )
+    net[ 'r3' ].cmd( 'ip addr add 192.168.4.1/24 brd + dev r3-eth2' )
+
+    # add IP Address for router 4
+    net[ 'r4' ].cmd( 'ip addr add 192.168.4.1/24 brd + dev r4-eth0' )
+    net[ 'r4' ].cmd( 'ip addr add 192.168.1.1/24 brd + dev r4-eth1' )
+    net[ 'r4' ].cmd( 'ip addr add 192.168.3.1/24 brd + dev r4-eth2' )
+
+    # Start IP Forward on Router
+    net[ 'r1' ].cmd( 'sysctl net.ipv4.ip_forward=1' )
+    net[ 'r2' ].cmd( 'sysctl net.ipv4.ip_forward=1' )
+    net[ 'r3' ].cmd( 'sysctl net.ipv4.ip_forward=1' )
+    net[ 'r4' ].cmd( 'sysctl net.ipv4.ip_forward=1' )
+
+    net.start()
+
+    # ping all host
+    info('\n', net.ping(), '\n')
 
     # Stop Network
     net.stop()
@@ -73,4 +82,4 @@ if __name__ == '__main__':
     os.system('mn -c')
     os.system( 'clear' )
     setLogLevel( 'info' )
-    coonfigNetwork()
+    configNetwork()
